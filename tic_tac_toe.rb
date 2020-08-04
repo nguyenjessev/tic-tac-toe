525fc4e7d7cb3f45af810921e73f3a34877fbaad
Jesse <nguyenjessev@gmail.com>
2020-08-04 19:06:03 -0400

    Add Game class, override Board and Cell to_s methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
 module TicTacToe
+  # This class represents te game instance
+  class Game
+    attr_reader :board
+
+    def initialize
+      @board = Board.new
+    end
+
+    private
+
+    def start_game
+      game_over = false
+
+      puts 'A new game has started!'
+
+      until game_over
+        board.display
+      end
+    end
+  end
+
   # This class represents the game board itself
   class Board
     attr_reader :board_size, :board
@@ -8,6 +29,13 @@ module TicTacToe
     def initialize(board_size = 3)
       @board_size = board_size
       build_board
+    end
+
+    def to_s
+      board.each_with_index do |row, index|
+        puts "#{row[0]} | #{row[1]} | #{row[2]}"
+        puts '- - - - -' unless index == 2
+      end
     end
 
     private
@@ -19,8 +47,14 @@ module TicTacToe
 
   # This class represents a cell on the game board
   class Cell
-    def initialize(status = '')
+    attr_accessor :status
+
+    def initialize(status = ' ')
       @status = status
+    end
+
+    def to_s
+      status
     end
   end
 end
